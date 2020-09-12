0999c27b1a085baa541802ffd785568bfcf204bc
Thetechguy <iliamanolovuni@gmail.com>
2020-09-12 15:22:18 +0300

    Updates

---
+++ precache-manifest.824059a63eef362fb14c0b741a10daab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a071e668821424e3e383c96d23aeb2f9",
+    "url": "/oxfordhack2020/index.html"
+  },
+  {
+    "revision": "c8ff172bf006d0e7276e",
+    "url": "/oxfordhack2020/static/css/main.c212b56e.chunk.css"
+  },
+  {
+    "revision": "7190dc1dd8cf7a6c6c02",
+    "url": "/oxfordhack2020/static/js/2.c6fa2861.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/oxfordhack2020/static/js/2.c6fa2861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ff172bf006d0e7276e",
+    "url": "/oxfordhack2020/static/js/main.d5bb9174.chunk.js"
+  },
+  {
+    "revision": "6d7a1e8888c11288cc20",
+    "url": "/oxfordhack2020/static/js/runtime-main.77d8e6f3.js"
+  },
+  {
+    "revision": "ca399e727838a837f164a68a82bacb4e",
+    "url": "/oxfordhack2020/static/media/no.ca399e72.png"
+  },
+  {
+    "revision": "2f1f1920d13320f0d31cc6cd05c267be",
+    "url": "/oxfordhack2020/static/media/yes.2f1f1920.png"
+  }
+]);

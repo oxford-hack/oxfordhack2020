4057a17c0877b7066d19d47d5516f78ea2d38fb0
Thetechguy <iliamanolovuni@gmail.com>
2020-09-20 15:13:44 +0300

    Updates

---
+++ precache-manifest.6cf293c6cd49992eeb1f04bf5e701638.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ad98f0242e7ced3f3ab829cafc1012",
+    "url": "/oxfordhack2020/index.html"
+  },
+  {
+    "revision": "e04678e5606f0e4ba822",
+    "url": "/oxfordhack2020/static/css/2.da69928d.chunk.css"
+  },
+  {
+    "revision": "8deb4fb6c92361129aed",
+    "url": "/oxfordhack2020/static/css/main.da11f351.chunk.css"
+  },
+  {
+    "revision": "e04678e5606f0e4ba822",
+    "url": "/oxfordhack2020/static/js/2.b3867b68.chunk.js"
+  },
+  {
+    "revision": "03409872514c006d1f265caa561274cc",
+    "url": "/oxfordhack2020/static/js/2.b3867b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8deb4fb6c92361129aed",
+    "url": "/oxfordhack2020/static/js/main.599cc8c6.chunk.js"
+  },
+  {
+    "revision": "6d7a1e8888c11288cc20",
+    "url": "/oxfordhack2020/static/js/runtime-main.77d8e6f3.js"
+  },
+  {
+    "revision": "04fefcd949676d6637397f04d3b6a3bf",
+    "url": "/oxfordhack2020/static/media/Alex haha.04fefcd9.JPG"
+  },
+  {
+    "revision": "7f12f863e1fbd6bc1f2a9329a492d3f9",
+    "url": "/oxfordhack2020/static/media/Alex normal.7f12f863.JPG"
+  },
+  {
+    "revision": "ed65b37cf9cc1f3f53d2bd5477121763",
+    "url": "/oxfordhack2020/static/media/Den_fun.ed65b37c.jpg"
+  },
+  {
+    "revision": "7f06518d03111d3813ce21f14cc847d4",
+    "url": "/oxfordhack2020/static/media/Den_normal.7f06518d.jpg"
+  },
+  {
+    "revision": "90ea81c4f311198d662f7e602aa5d9be",
+    "url": "/oxfordhack2020/static/media/Ilia_fun.90ea81c4.png"
+  },
+  {
+    "revision": "070ab2f17b479882fce6dcb3fd44d0cc",
+    "url": "/oxfordhack2020/static/media/Ilia_normal.070ab2f1.png"
+  },
+  {
+    "revision": "056ed4086c32ba02ac7f43d9d38210e6",
+    "url": "/oxfordhack2020/static/media/Jonny fun.056ed408.jpg"
+  },
+  {
+    "revision": "6ba74c4d80b8edc84bccf577538385ee",
+    "url": "/oxfordhack2020/static/media/Jonny normal.6ba74c4d.jpg"
+  },
+  {
+    "revision": "d440e36420410d6a831489d7b81895fd",
+    "url": "/oxfordhack2020/static/media/Serbi haha.d440e364.jpg"
+  },
+  {
+    "revision": "32ac9e84ee7703dd74d4940e7f64d0d4",
+    "url": "/oxfordhack2020/static/media/Serbi normal.32ac9e84.jpg"
+  },
+  {
+    "revision": "09c71fadbf22551fc315816c2d9ca39c",
+    "url": "/oxfordhack2020/static/media/Sonia 1.09c71fad.jpg"
+  },
+  {
+    "revision": "e0740ef317d1bca6d73aac2ea0222120",
+    "url": "/oxfordhack2020/static/media/Sonia2.e0740ef3.jpg"
+  },
+  {
+    "revision": "aca02bf363671350725d205871787637",
+    "url": "/oxfordhack2020/static/media/bgskyline.aca02bf3.png"
+  },
+  {
+    "revision": "7b5fedbae47ca41ccfa9f11338dc6a79",
+    "url": "/oxfordhack2020/static/media/logo.7b5fedba.png"
+  },
+  {
+    "revision": "7213a8da3e0a29c2d67ea7b93921bfdc",
+    "url": "/oxfordhack2020/static/media/twoo-flying.7213a8da.gif"
+  },
+  {
+    "revision": "d7acc01c4cb9307625fad835906a1c39",
+    "url": "/oxfordhack2020/static/media/twoo-flying.d7acc01c.webm"
+  },
+  {
+    "revision": "a53caeb31440242437dcf2af27846352",
+    "url": "/oxfordhack2020/static/media/twoo_event_tmp.a53caeb3.png"
+  },
+  {
+    "revision": "19762891d9c07539999bd5f0ba991ac1",
+    "url": "/oxfordhack2020/static/media/twoo_faq.19762891.gif"
+  },
+  {
+    "revision": "720506468bc97c774d298a866863c4fa",
+    "url": "/oxfordhack2020/static/media/twoo_faq.72050646.webm"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oxfordhack2020/precache-manifest.e10d46ef8bfafe3704760b594e54861e.js"
+  "/oxfordhack2020/precache-manifest.6cf293c6cd49992eeb1f04bf5e701638.js"
 );
 
 self.addEventListener('message', (event) => {
